86cdcd0edeaa49b8b63ae3b0b6231ed6dc5b3249
jarilton <jariltonpereira02@hotmail.com>
2022-09-24 20:09:57 -0300

    add loading no botão alugar agora até fazer o post

--- src/screens/SchedulingDetails/index.tsx
+++ src/screens/SchedulingDetails/index.tsx
@@ -54,6 +54,7 @@ interface rentalPeriod {
 }
 
 export function SchedulingDetails() {
+  const [loading, setLoading] = useState(false);
   const [rentalPeriod, setRentalPeriod] = useState<rentalPeriod>(
     {} as rentalPeriod
   );
@@ -66,6 +67,8 @@ export function SchedulingDetails() {
   const rentTotal = Number(dates.length * car.rent.price);
 
   async function handleConfirmRental() {
+    setLoading(true);
+
     const schedulesByCar = await api.get(`/schedules_bycars/${car.id}`);
 
     const unavailable_dates = [
@@ -89,7 +92,10 @@ export function SchedulingDetails() {
         unavailable_dates,
       })
       .then(() => navigation.navigate("SchedulingComplete"))
-      .catch(() => Alert.alert("Não foi possivel confirmar o agendamento."));
+      .catch(() => {
+        Alert.alert("Não foi possivel confirmar o agendamento.");
+        setLoading(false);
+      });
   }
 
   function handleBack() {
@@ -179,6 +185,8 @@ export function SchedulingDetails() {
           title="Alugar agora"
           color={theme.colors.success}
           onPress={handleConfirmRental}
+          enabled={!loading}
+          loading={loading}
         />
       </Footer>
     </Container>
