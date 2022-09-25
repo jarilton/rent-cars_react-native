8919d13bc73feb6d77221be557cf3452d1d15395
jarilton <jariltonpereira02@hotmail.com>
2022-09-25 17:07:38 -0300

    Adicionando o slider dos carros

--- src/components/ImageSlider/index.tsx
+++ src/components/ImageSlider/index.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { FlatList } from "react-native";
 
 import {
   Container,
@@ -16,18 +17,22 @@ export function ImageSlider({ ImagesUrl }: Props) {
   return (
     <Container>
       <ImageIndexes>
-        <ImageIndex active={true} />
-        <ImageIndex active={false} />
-        <ImageIndex active={false} />
-        <ImageIndex active={false} />
+        {ImagesUrl.map((_, index) => (
+          <ImageIndex key={String(index)} active={true} />
+        ))}
       </ImageIndexes>
 
-      <CarImageWrapper>
-        <CarImage 
-            source={{ uri: ImagesUrl[0]}}
-            resizeMode="contain"
-        />
-      </CarImageWrapper>
+      <FlatList
+        data={ImagesUrl}
+        keyExtractor={(key) => key}
+        renderItem={({ item }) => (
+          <CarImageWrapper>
+            <CarImage source={{ uri: item }} resizeMode="contain" />
+          </CarImageWrapper>
+        )}
+        horizontal
+        showsHorizontalScrollIndicator={false}
+      />
     </Container>
   );
 }
