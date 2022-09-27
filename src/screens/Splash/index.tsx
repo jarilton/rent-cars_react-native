949d982472c610d901ab3b37da41f48c78377ec1
jarilton <jariltonpereira02@hotmail.com>
2022-09-26 23:22:32 -0300

    Configurando nossas próprias transições

--- src/screens/Splash/index.tsx
+++ src/screens/Splash/index.tsx
@@ -1,23 +1,34 @@
 import React from "react";
-import { Button, StyleSheet } from "react-native";
+import { Button, StyleSheet, Dimensions } from "react-native";
 import Animated, {
   useSharedValue,
   useAnimatedStyle,
+  withTiming,
+  Easing,
 } from "react-native-reanimated";
 
 import { Container } from "./styles";
+
+const WIDTH = Dimensions.get("window").width;
 
 export function Splash() {
   const animation = useSharedValue(0);
 
   const animatedStyles = useAnimatedStyle(() => {
     return {
-      transform: [{ translateX: animation.value }],
+      transform: [
+        {
+          translateX: withTiming (animation.value, {
+            duration: 2000,
+            easing: Easing.bezier(0,1.55,1,-0.45)
+          }),
+        },
+      ],
     };
   });
 
   function handleAnimationPosition() {
-    animation.value = Math.random() * 150;
+    animation.value = Math.random() * (WIDTH - 100);
   }
 
   return (
